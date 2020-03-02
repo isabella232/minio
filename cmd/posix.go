6da554caa4453c1e7572c4a6ec729e631532f0bd
Kody Kantor <kody@kkantor.com>
2020-03-02 20:52:43 +0000

    add fsync at the end of object writes

--- cmd/posix.go
+++ cmd/posix.go
@@ -1163,6 +1163,11 @@ func (s *posix) CreateFile(volume, path string, fileSize int64, r io.Reader) (er
 		return errMoreData
 	}
 
+	err = w.Sync()
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
