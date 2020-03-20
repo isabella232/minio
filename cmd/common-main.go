5bf52277094fae15ba3b2cfca025181538a6cdfc
Kody A Kantor <kody@kkantor.com>
2020-03-20 14:13:37 -0500

    disable server-side encryption

--- cmd/common-main.go
+++ cmd/common-main.go
@@ -224,7 +224,8 @@ func handleCommonEnvVars() {
 				"Unable to validate credentials inherited from the shell environment")
 		}
 		globalActiveCred = cred
-		globalConfigEncrypted = true
+		//globalConfigEncrypted = true
+		globalConfigEncrypted = false
 	}
 
 	if env.IsSet(config.EnvAccessKeyOld) && env.IsSet(config.EnvSecretKeyOld) {
